5939a8a9a6d18d2de97286457dc9073cec2978d8
Saad bin abid <saad.abid@crowdbotics.com>
2022-10-03 14:41:22 +0500

    update options

--- options/options.js
+++ options/options.js
@@ -2,9 +2,9 @@ import { StyleSheet } from "react-native"
 
 export const globalOptions = {
   name: "ecommerce_37121",
-  url: "https://drone-express-36671.botics.co",
-  api: "https://drone-express-36671.botics.co/api/v1",
-  oscar_countries: "https://drone-express-36671.botics.co/api/countries/US/",
+  url: "https://ecommerce_37121.botics.co",
+  api: "https://ecommerce_37121.botics.co/api/v1",
+  oscar_countries: "https://ecommerce_37121.botics.co/api/countries/US/",
   partner_id: 4,
 }
 export const modulesOptions = {
